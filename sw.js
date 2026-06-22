73ab5856dbc6d5a117b45b5d027966c02cd5c28f
leodbs813-eng <leodbs813@gmail.com>
2026-06-22 16:46:41 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -13,16 +13,3 @@ const urlsToCache = [
   "./someone/hertz3.PNG",
   "./someone/hertz4.PNG"
 ];
-
-self.addEventListener("install", (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
-  );
-});
-
-self.addEventListener("fetch", (event) => {
-  event.respondWith(
-    caches.match(event.request).then(response => response || fetch(event.request))
-  );
-});
-이 구조인데
