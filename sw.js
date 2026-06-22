9b815b0912a2b7386aa7a4cb9735a936d9ce9684
leodbs813-eng <leodbs813@gmail.com>
2026-06-22 16:50:23 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -13,3 +13,17 @@ const urlsToCache = [
   "./someone/hertz3.PNG",
   "./someone/hertz4.PNG"
 ];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
